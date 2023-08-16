05a56ed5b7fc301e99c8ebf359311651fc0b70f2
maddy <mdeason89@gmail.com>
2023-08-16 21:51:24 +0900

    Updates

--- _app/immutable/chunks/paths.93d9b8c4.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1nedx91)==null?void 0:s.base)??"/maddyscodes.github.io";var e;const t=((e=globalThis.__sveltekit_1nedx91)==null?void 0:e.assets)??a;export{t as a,a as b};
